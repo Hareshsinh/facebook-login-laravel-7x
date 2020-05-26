84f2d3ee8e18e0e7bfc755e7b7cd3517b4fe3cf0
hareshsinh <haresh.sisodiya@viitor.cloud>
2020-05-26 17:44:45 +0530

    add changes

---
+++ config/services.php
@@ -0,0 +1,71 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+    ],
+
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
+    ],
+
+    'ses' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
+
+    'facebook' => [
+        'active' => env('FACEBOOK_ACTIVE'),
+        'client_id' => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect' => env('FACEBOOK_REDIRECT'),
+        'scopes' => [],
+        'with' => [],
+        'fields' => [],
+    ],
+
+//    'google' => [
+//        'active' => env('GOOGLE_ACTIVE'),
+//        'client_id' => env('GOOGLE_CLIENT_ID'),
+//        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+//        'redirect' => env('GOOGLE_REDIRECT'),
+//        'scopes' => [],
+//        'with' => [],
+//    ],
+//
+//    'linkedin' => [
+//        'active' => env('LINKEDIN_ACTIVE'),
+//        'client_id' => env('LINKEDIN_CLIENT_ID'),
+//        'client_secret' => env('LINKEDIN_CLIENT_SECRET'),
+//        'redirect' => env('LINKEDIN_REDIRECT'),
+//        'scopes' => [],
+//        'with' => [],
+//        'fields' => [],
+//    ],
+//
+//    'twitter' => [
+//        'active' => env('TWITTER_ACTIVE'),
+//        'client_id' => env('TWITTER_CLIENT_ID'),
+//        'client_secret' => env('TWITTER_CLIENT_SECRET'),
+//        'redirect' => env('TWITTER_REDIRECT'),
+//        'scopes' => [],
+//        'with' => [],
+//    ],
+
+];
